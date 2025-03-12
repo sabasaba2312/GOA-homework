524cbade1865eb33df81f4369b0f0a9ba0213f57
sabatavartkiladze <sabapro662@gmail.com>
2025-03-12 13:53:54 +0400

    second commit

---
+++ day 10/homework/hw1.py
@@ -0,0 +1,22 @@
+#თიოთეული მონაცემთა ტიპისთვის (str,int,float), შექმენით 5 ცვლადი და დაუწერეთ კომენტარი თუ რომელ მონაცემთა ტიპს ინახავს ცვლადი
+
+#string ცვლადები
+name = "saba"
+surname = "tavartkiladze"
+hobby = "footballer"
+location = "xulo"
+laptop = "hp"
+
+#integer ცვლადები
+age = 15
+number = 20
+number1 = 40
+ram = 8
+gb = 512
+
+#float ცვლადები
+height = 1.72
+wona = 64.2
+number7 = 34.8
+number8 = 98.9
+number10 = 100.6
